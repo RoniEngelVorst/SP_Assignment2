069d8ccd03b1edd29d73f1633db5a549d7318ea7
RoniEngelVorst <roniengel12@gmail.com>
2024-02-07 23:46:39 +0200

    fixing some bugs part 1

--- my_mat.c
+++ my_mat.c
@@ -2,10 +2,15 @@
 #include "my_mat.h"
 #define SIZE 10
 
+void inputMatrix(int mat[SIZE][SIZE]);
+int hasPath(int mat[SIZE][SIZE]);
+int shortestPath(int mat[SIZE][SIZE]);
+
+
+
 void inputMatrix(int mat[SIZE][SIZE]){
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
-            printf("Enter value for mat[%d][%d]: ", i, j);
             scanf("%d", &mat[i][j]);
         }
     }
@@ -14,26 +19,32 @@ void inputMatrix(int mat[SIZE][SIZE]){
 
 int hasPathHelper(int mat[SIZE][SIZE], int first, int last, int beenThere[SIZE]){
     if(first == last){
-        return 1;
+        if(mat[first][last] == 0){
+            return 0;
+        }
+        else{
+            return 1;
+        }
     }
     
     beenThere[first] = 1;
-    for(int i = 1; i<SIZE; i++){
+    for(int i = 0; i < SIZE; i++){
         if(mat[first][i] != 0 && beenThere[i] != 1){
             if(hasPathHelper(mat, i, last, beenThere)){
                 return 1;
             }
         }
     }
-    return 0;
+    
+    return 0; 
+   
 }
+
 
 
 int hasPath(int mat[SIZE][SIZE]){
     int first, last;
-    printf("Enter the first node number: ");
     scanf("%d", &first);
-    printf("Enter the second node number: ");
     scanf("%d", &last);
     int beenThere[SIZE] = {0};
     int ans = hasPathHelper(mat, first, last, beenThere);
@@ -51,9 +62,22 @@ int hasPath(int mat[SIZE][SIZE]){
 
 int shortestPath(int mat[SIZE][SIZE]){
     int D_mat[SIZE][SIZE] = {0};
+
     for(int i = 0; i < SIZE; i++){
-        for(int j = i; j < SIZE; j++){
-            D_mat[i][j] = mat[i][j];
+        D_mat[i][i] = 0;
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            D_mat[i][j] = mat[i][j]; 
+        }
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (i != j && mat[i][j] == 0) {
+                D_mat[i][j] = 0;
+            }
         }
     }
 
@@ -70,9 +94,7 @@ int shortestPath(int mat[SIZE][SIZE]){
         }
     }
     int first, last;
-    printf("Enter the first node number: ");
     scanf("%d", &first);
-    printf("Enter the second node number: ");
     scanf("%d", &last);
     if(D_mat[first][last] == 0){
         printf("%d", -1);
